fc8e430e818f11f9ae0b69ce4df2f3dd4f530aa3
João <jvpessoa10@hotmail.com>
2018-08-29 23:04:34 -0300

    finalização do front

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,10 +9,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
-  }
   
+    
 
 
-
+  };
 }
